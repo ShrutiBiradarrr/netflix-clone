25ec37127b1bd2480024606e623b676efe2bb540
shrutii1010 <shruti.biradar1010@gmail.com>
2022-10-01 12:20:36 +0530

    updated

--- src/component/Row.jsx
+++ src/component/Row.jsx
@@ -1,42 +1,53 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios';
-import requests from '../Request';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import requests from "../Request";
+import Movie from "./Movie";
+//import {MdChevronLeft , MdChevronRight} from 'react-icons/md'
 
+function Row({ title, fetchURL ,rowID }) {
+  const [movies, setMovies] = useState([]);
+  console.log(title, fetchURL);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(fetchURL);
+        console.log(data);
+        setMovies(data.results);
+      } catch (error) {
+        console.log("error in featching the data", error.message);
+      }
+    };
+    fetchData();
+  }, [fetchURL]);
+  const sliderLeft=()=>{
+    var slider = document.getElementById('slider' + rowID)
+    slider.scrollLeft = slider.scrollLeft - 500;
+  }
+  const sliderRight=()=>{
+    var slider = document.getElementById('slider' + rowID)
+    slider.scrollRight = slider.scrollRight + 500;
+  }
 
-function Row({ title, featchURL}) {
-
-    const [movies, setMovies] = useState([])
-    console.log(title);
-    useEffect(() => {
-     const fetchData = async()=>{
-        try {
-            const {data} = await axios.get(featchURL)
-            console.log(data);
-            setMovies(data.results);
-        } catch (error) {
-            console.log("error in featching the data", error.message);
-
-        }
-     };
-     fetchData();
-    }, [featchURL])
-
-    return (
-        <>
-        <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
-        <div className='relative flex item-center' >
-            <div id={'slider'}>
-                  {movies.map((ele,index)=>{
-                    <div>
-                        <img src={`https://image.tmdb.org/t/p/w500/${ele?.backdrop_path}`}  alt='' /> 
-                    </div>
-                    // if Element is null then src is empty
-                    //ele===null? "":ele ///orr//ele?.ele
-                  })}
-            </div>
+  return (
+    <>
+      <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+      <div className="relative flex item-center  ">
+        {/* <MdChevronLeft  */}
+        {/* onClick={sliderLeft} className="bg-white rounded-full left-0 absolute opacity-50 hover:opacity-100 cursor-pointer z-20  group-hover:block" size={40} /> */}
+        <div id={"slider" + rowID} className="overflow-x-scroll overflow-y-hidden flex  whitespace-nowrap scroll-smooth scrollbar-hide relative">
+          {movies.map((ele, index) => (
+            
+             <Movie key={index} ele={ele}/>
+           
+            // if Element is null then src is empty
+            //ele===null? "":ele ///orr//ele?.ele
+          ))}
         </div>
-        </>
-    )
+        {/* <MdChevronRight 
+        onClick={sliderRight} className="bg-white rounded-full right-0 absolute opacity-50 hover:opacity-100 cursor-pointer z-20  group-hover:block" size={40}/> */}
+      </div>
+    </>
+  );
 }
 
 export default Row;
